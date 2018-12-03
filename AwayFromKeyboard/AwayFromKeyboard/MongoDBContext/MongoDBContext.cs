db4b160affc645faa8fed30c581cbaef64155298
ChadJ <ChadJ@INTERNBOX1>
2018-12-03 15:26:40 +0200

    Update After Loosing everything

---
+++ AwayFromKeyboard/AwayFromKeyboard/MongoDBContext/MongoDBContext.cs
@@ -0,0 +1,39 @@
+﻿using AwayFromKeyboard.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace AwayFromKeyboard.MongoDBContext
+{
+    public class MongoDBContext
+    {
+        public const string CONNECTION_STRING_NAME = "connectionString";
+        public const string DATABASE_NAME = "AFKdb";
+        public const string EMPLOYEE_COLLECTION = "Employee";
+
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _database;
+
+        static MongoDBContext()
+        {
+            string constr = ConfigurationManager.AppSettings[CONNECTION_STRING_NAME];
+            _client = new MongoClient(constr);
+            _database = _client.GetDatabase("AFKdb");
+        }
+
+        public IMongoClient Client
+        {
+            get { return _client; }
+        }
+
+        public IMongoCollection<Employee> Employees
+        {
+            get { return _database.GetCollection<Employee>(EMPLOYEE_COLLECTION); }
+        }
+
+
+    }
+}
